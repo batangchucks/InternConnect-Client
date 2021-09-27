c62bda42a51462918bad0259854689efdb9d0664
batangchucks <kylagail.azucena.iics@ust.edu.ph>
2021-09-27 22:30:45 +0800

    Changes to status page

--- src/app/pages/landing/components/status/status.component.ts
+++ src/app/pages/landing/components/status/status.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatusComponent implements OnInit {
 
+  FormEntry: boolean = false;
+  CompanyEntry: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  toUpdate() {
+    this.FormEntry = true;
+  }
+
+  toClose() {
+    this.FormEntry = false;
+  }
+
+  toChange() {
+    this.CompanyEntry = true;
+  }
+
+  toCloseSecond() {
+    this.CompanyEntry = false;
+  }
+
 
 }
